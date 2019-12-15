3ff49fd26dae3282289e32c8199d0e5a3ba8734c
Casey Yang <cmoldavon@gmail.com>
2019-12-15 13:34:51 -0800

    Updating style.css, adding UI path flow & adding Slider

---
+++ design.js
@@ -0,0 +1,40 @@
+// This page is for design based functions and code
+
+
+    $("#search-button").on("click", function(){
+
+        $("#city-container").fadeIn(2000);
+        $("#five-day").toggle(3000);
+        // $(".day-container").fadeIn(1000);
+
+        $("").toggle(3000);
+        $("#day-2").toggle(3000);
+        $("#day-3").toggle(3000);
+        $("#day-4").toggle(3000);
+        $("#day-5").toggle(3000);
+    
+
+
+    });
+
+
+
+    // list of actions:
+
+    // - page 1 to page 2 fade transition (after search button is pressed)
+    //     - page 1 becomes visibility: hidden
+    //     - page 2 becomes visibility: visible (add fade effect)
+
+    // - Home button becomes visibility: visible
+    // - monster (image-display-2) toggles (becomes visibility: visible)
+    //     - slow toggle appear (for comedy purposes)
+    // - speech bubble 1 appears
+    //     - becomes visibility: visible
+    // - speech bubble 2 appears
+    //     - becomes visibility: visible
+    // - speech bubble 3 appears
+    //     - becomes visibility: visible
+
+    // - create event listener for monster (image-display-2)
+    //     - on "click" reset back to page 1
+
