811dd4fa09eb959a1bc607313ab89b0cbd1655ba
Casey Yang <cmoldavon@gmail.com>
2019-12-17 10:50:14 -0800

    Updating style.css with changes to container sizes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 // ========================= This is the Map onload =========================
 var userLat = "";
 var userLong = "";
@@ -29,94 +28,63 @@ setTimeout(function () {
 }, 10000);
 
 
-// ========================= Search listing =========================
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 // ========================= Design animations/displays + page shifts =========================
-$("#search-button").on("click", function(event) {
-    event.preventDefault();
+$(document).ready(function () {
+    $("#search-button").on("click", function (event) {
+        event.preventDefault();
 
-    $("#page-2").fadeIn(2000, function(){
-        $(this).css("visibility", "visible");
+        $("#page-2").fadeIn(3000, function () {
+            $(this).css("visibility", "visible");
+        });
+
+        $("#home-tab").fadeIn(3000, function () {
+            $(this).css("visibility", "visible");
+        });
+
+        $("#image-display-2").delay(3000).show("slide", { direction: "left" }, 1000);
+        $("#bubble-1").delay(4000).fadeIn(4200);
+        $("#bubble-2").delay(4500).fadeIn(4200);
+        $("#bubble-3").delay(5000).fadeIn(4200);
+
+        getmap();
     });
 
-    $("#home-tab").fadeIn(2000, function(){
-        $(this).css("visibility", "visible");
+
+    // idea: monster pops up over page while content loats and then disappears
+
+
+
+
+
+
+    $("#home-tab").on("click", function () {
+
+        $("#page-2").fadeOut(2000)
+        // , function(){
+        // $(this).css("display", "none");
+        // });
+
+        // $("#home-tab").fadeOut(2000, function(){
+        //     $(this).css("display", "none");
+        // });
+
+        // $("#image-display-2").fadeOut(2000, function(){
+        //     $(this).css("display", "none");
+        // });
+
+        // $("#bubble-1").fadeOut(2000, function(){
+        //     $(this).css("display", "none");
+        // });
+
+        // $("#bubble-2").fadeOut(2000, function(){
+        //     $(this).css("display", "none");
+        // });
+
+        // $("#bubble-3").fadeOut(2000, function(){
+        //     $(this).css("display", "none");
+        // });
     });
-
-    $("#image-display-2").delay(3000).show("slide", {direction: "left" }, 1000);
-    $("#bubble-1").delay(4000).fadeIn(4200);
-    $("#bubble-2").delay(4500).fadeIn(4200);
-    $("#bubble-3").delay(5000).fadeIn(4200);
-
-    getmap();
 });
-
-
-// idea: monster pops up over page while content loats and then disappears
-
-
-// $("#home-tab").on("click", function(event) {
-//     event.preventDefault();
-
-//     $("#page-2").fadeOut(2000, function(){
-//         $(this).css("display", "none");
-//     });
-
-    // $("#home-tab").fadeOut(2000, function(){
-    //     $(this).css("display", "none");
-    // });
-
-    // $("#image-display-2").fadeOut(2000, function(){
-    //     $(this).css("display", "none");
-    // });
-
-    // $("#bubble-1").fadeOut(2000, function(){
-    //     $(this).css("display", "none");
-    // });
-
-    // $("#bubble-2").fadeOut(2000, function(){
-    //     $(this).css("display", "none");
-    // });
-
-    // $("#bubble-3").fadeOut(2000, function(){
-    //     $(this).css("display", "none");
-    // });
-// });
