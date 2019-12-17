8095020d17b8fe4ed2f283db84245f7f8ef38006
Casey Yang <cmoldavon@gmail.com>
2019-12-16 17:37:18 -0800

    Updating display animations in style.css and script.js

--- script.js
+++ script.js
@@ -1,29 +1,132 @@
 
-// //GET USER GEOLOCATION
-// function getLocation() {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(showPosition);
-//   } else { 
-//     x.innerHTML = "Geolocation is not supported by this browser.";
-//   }
-// }
-// function showPosition(position) {
-// console.log(position.coords.latitude);
-// console.log(position.coords.longitude);
-// }
-// //NOT YET WORKING - position.coord etc returns as undefined
-// function GetMap(position) {
-//         var map = new Microsoft.Maps.Map('#myMap', {
-//             credentials: 'ArfOhekfSK9rly4qjcdt20SypfRiLnIYtUbxAzrv6-PDjseOmmMguOsqBYcrD-sW',
-//             center: new Microsoft.Maps.Location(position.coords.latitude, position.coords.longitude)
-//         });
-//         var center = map.getCenter();
-//         //Create custom Pushpin
-//         var pin = new Microsoft.Maps.Pushpin(center, {
-//             title: 'Microsoft',
-//             subTitle: 'City Center',
-//             text: '1'
-//         });
-//         //Add the pushpin to the map
-//         map.entities.push(pin);
-//     }
+
+// ========================= This is the Map onload =========================
+var userLat = "";
+var userLong = "";
+var userLocationGlobal = "";
+function GetMap() {
+    var map = new Microsoft.Maps.Map('#myMap', {
+        credentials: "ArfOhekfSK9rly4qjcdt20SypfRiLnIYtUbxAzrv6-PDjseOmmMguOsqBYcrD-sW"
+    });
+    //Request the user's location
+    navigator.geolocation.getCurrentPosition(function (position) {
+        var loc = new Microsoft.Maps.Location(
+            position.coords.latitude,
+            position.coords.longitude);
+        userLat = position.coords.latitude;
+        userLong = position.coords.longitude;
+        userLocationGlobal = loc;
+        //Add a pushpin at the user's location.
+        var pin = new Microsoft.Maps.Pushpin(loc);
+        map.entities.push(pin);
+        //Center the map on the user's location.
+        map.setView({ center: loc, zoom: 15 });
+    });
+}
+setTimeout(function () {
+    console.log(userLong);
+    console.log(userLat);
+}, 10000);
+
+
+// ========================= Search listing =========================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ========================= Design animations/displays + page shifts =========================
+$("#search-button").on("click", function(event) {
+    event.preventDefault();
+
+    $("#page-2").fadeIn(2000, function(){
+        $(this).css("visibility", "visible");
+    });
+
+    $("#home-tab").fadeIn(2000, function(){
+        $(this).css("visibility", "visible");
+    });
+
+    $("#image-display-2").delay(3000).show("slide", {direction: "left" }, 1000);
+    $("#bubble-1").delay(4000).fadeIn(4200);
+    $("#bubble-2").delay(4500).fadeIn(4200);
+    $("#bubble-3").delay(5000).fadeIn(4200);
+
+    getmap();
+});
+
+
+// idea: monster pops up over page while content loats and then disappears
+
+
+// $("#home-tab").on("click", function(event) {
+//     event.preventDefault();
+
+//     $("#page-2").fadeOut(2000, function(){
+//         $(this).css("display", "none");
+//     });
+
+    // $("#home-tab").fadeOut(2000, function(){
+    //     $(this).css("display", "none");
+    // });
+
+    // $("#image-display-2").fadeOut(2000, function(){
+    //     $(this).css("display", "none");
+    // });
+
+    // $("#bubble-1").fadeOut(2000, function(){
+    //     $(this).css("display", "none");
+    // });
+
+    // $("#bubble-2").fadeOut(2000, function(){
+    //     $(this).css("display", "none");
+    // });
+
+    // $("#bubble-3").fadeOut(2000, function(){
+    //     $(this).css("display", "none");
+    // });
+// });
+
+
+
+
+    // $("#home-tab").show(1000);
+    // $("#image-display-2").slideLeft(3000);
+
+    // $("#bubble-1").delay(5000).fadeIn(1000);
+    // $("#bubble-2").delay(6000).fadeIn(1000);
+    // $("#bubble-3").delay(7000).fadeIn(1000);
